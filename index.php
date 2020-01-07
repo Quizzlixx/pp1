e33825f981ef4edc9ed6113fe138f3d853ff5b62
Kerrie Low <klow4@mail.greenriver.edu>
2020-01-07 12:15:54 -0500

    +index.php created./n +Error reporting on./n +Title added/n +Header added. +Copied bootstrap from personal template

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Kerrie Low
+ * 1.7.20
+ * URL: https://github.com/Quizzlixx/pp1.git
+ */
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pair Program 1</title>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+<!-- header -->
+<div class="container-fluid jumbotron jumbotron-fluid">
+    <h2 class="text-center text-monospace">Pair Program 1</h2>
+    <div class="container d-flex justify-content-between">
+        <p>index.php</p>
+        <p class="lead">Written by Kerrie Low</p>
+    </div>
+</div> <!-- /header -->
+<div class="container">
+
+</div>
+<!-- jQuery -->
+<!-- slim jQuery does not support AJAX -->
+<script src="//code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="//stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
